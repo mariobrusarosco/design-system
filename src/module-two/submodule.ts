6ccbd8a0794d1b96decc839a78375b1eb9f6b670
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-05-14 07:55:20 -0300

    exploring...

--- src/module-two/submodule.ts
+++ src/module-two/submodule.ts
@@ -1,3 +1,7 @@
-export const mySubModuleTwo = () => {
-  console.log("Hello World Two/sub");
+export interface MyInterface {
+  name: string;
+}
+
+export const mySubModuleTwo = (test: MyInterface) => {
+  console.log("Hello World Two/sub", test);
 };
